5d4831d757770947c06036a8cf718591005d796a
Guillaume Duc <guillaume@guiduc.org>
2025-12-14 09:45:00 +0100

    Day 11 part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -5,9 +5,49 @@ use pathfinding::prelude::count_paths;
 fn solve_part1(puzzle: &HashMap<String, Vec<String>>) -> usize {
     count_paths(
         "you".to_owned(),
-        |n| puzzle.get(n).unwrap().iter().cloned(),
+        |n| puzzle.get(n).unwrap_or(&Vec::new()).clone(),
         |n| *n == "out",
     )
+}
+
+fn solve_part2(puzzle: &HashMap<String, Vec<String>>) -> usize {
+    let svrdac = count_paths(
+        "svr".to_owned(),
+        |n| puzzle.get(n).unwrap_or(&Vec::new()).clone(),
+        |n| *n == "dac",
+    );
+
+    let svrfft = count_paths(
+        "svr".to_owned(),
+        |n| puzzle.get(n).unwrap_or(&Vec::new()).clone(),
+        |n| *n == "fft",
+    );
+
+    let fftdac = count_paths(
+        "fft".to_owned(),
+        |n| puzzle.get(n).unwrap_or(&Vec::new()).clone(),
+        |n| *n == "dac",
+    );
+
+    let dacfft = count_paths(
+        "dac".to_owned(),
+        |n| puzzle.get(n).unwrap_or(&Vec::new()).clone(),
+        |n| *n == "fft",
+    );
+
+    let dacout = count_paths(
+        "dac".to_owned(),
+        |n| puzzle.get(n).unwrap_or(&Vec::new()).clone(),
+        |n| *n == "out",
+    );
+
+    let fftout = count_paths(
+        "fft".to_owned(),
+        |n| puzzle.get(n).unwrap_or(&Vec::new()).clone(),
+        |n| *n == "out",
+    );
+
+    svrdac * dacfft * fftout + svrfft * fftdac * dacout
 }
 
 fn parse_input(input: &str) -> HashMap<String, Vec<String>> {
@@ -28,4 +68,5 @@ fn main() {
     let input = include_str!("../input");
     let puzzle = parse_input(input);
     println!("Part 1 = {}", solve_part1(&puzzle));
+    println!("Part 2 = {}", solve_part2(&puzzle));
 }
